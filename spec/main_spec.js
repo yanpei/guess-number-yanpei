0b88a1bd7e545500bda6001f3241bfe8e46c1e2d
yanpei <1159319529@qq.com>
2016-02-28 14:03:11 +0800

    answer3

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -53,36 +53,39 @@ describe("测试猜数字游戏", function(){
         expect(expect_string).to.equal(result);
     });
 
-　//测试answerGenerator函数
-　it("测试随机生成的是一个数",function(){
-          var result = typeof(main.answerGenerator());
-          var expect_string = 'number';
-          expect(expect_string).to.equal(result);
-    });
-    it("测试随机生成的是一个四位数",function(){
-              var result = main.answerGenerator()>=1000&&main.answerGenerator()<=9999;
-              console.log("result:  "+result);
-              var expect_string = true;
-              expect(expect_string).to.equal(result);
+  //测试answerGenerator函数
+  　it("测试随机生成的是一个数",function(){
+            var result = typeof(main.answerGenerator());
+            var expect_string = 'number';
+            expect(expect_string).to.equal(result);
       });
+      it("测试随机生成的是一个四位数",function(){
+                var result = main.answerGenerator()>=1000&&main.answerGenerator()<=9999;
+                var expect_string = true;
+                expect(expect_string).to.equal(result);
+        });
 
       it("测试随机生成数每位都不重复",function(){
                 var answer = main.answerGenerator();
                 var answerArr = answer.toString().split("");
-                console.log("answerArr:  "+answerArr);
                 var result = (answerArr[0]!=answerArr[1]&&answerArr[0]!=answerArr[2]&&answerArr[0]!=answerArr[3]&&answerArr[1]!=answerArr[2]&&answerArr[1]!=answerArr[3]&&answerArr[2]!=answerArr[3]);
                 var expect_string = true;
                 expect(expect_string).to.equal(result);
         });
 
-/*
-    it("测试用例2", function(){
 
-        main();
-        var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '';
+//测试guess函数，mock 测试
+        it("测试guess函数",function(){
+               sinon.spy(console,"log");
+               sinon.stub(main,"answerGenerator").returns(1234);
+               sinon.stub(main,"compareNumber").returns("4A0B");
+               main.guess(1234);
+               expect(main.answerGenerator).to.have.been.calledWith();
+               expect(main.compareNumber).to.have.been.calledWith(1234,1234);
 
-        expect(expect_string).to.equal(result);
-    });
-    */
+               var result = _.flatten(console.log.args).join("\n");
+               var expect_string = "4A0B";
+               expect(expect_string).to.equal(result);
+        });
+
 });
